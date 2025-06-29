61468a4ed4b7e902126c4e84a658c03cc93cab48
Igor <igoralves@cambioreal.com>
2025-06-29 15:40:55 -0300

    refactor(jogador): method to include city adjacency check

--- domain/jogador.py
+++ domain/jogador.py
@@ -3,8 +3,11 @@ class Jogador:
         self.posicao = posicao
 
     def moverJogador(self, cidade):
-        self.posicao = cidade
-        print(f"Jogador movido para {cidade.nome}")
+        if cidade in self.posicao.vizinhacidade:
+            self.posicao = cidade
+            print(f"Jogador agora está em {self.posicao.nome}.")
+        else:
+            print(f"A cidade {cidade.nome} não é vizinha de {self.posicao.nome}.")
         
     def teletransportar(self, cidade):
         self.posicao = cidade
